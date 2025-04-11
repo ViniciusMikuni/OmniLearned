fcfa24619379e9f0fc32ccff80b2d38e491d6830
ViniciusMikuni <vinicius.mikuni@gmail.com>
2025-04-11 10:36:08 -0700

    modify benchmarks in job submission

--- train.sh
+++ train.sh
@@ -4,28 +4,29 @@ module load pytorch
 # for DDP
 export MASTER_ADDR=$(hostname)
 
-# #Very Big PET  Total params: 654.88M
-# cmd="omnilearned train  -o ./ --save-tag pretrain --dataset pretrain --use-pid --use-add --num-classes 201 --batch 64 --iterations 1000 --mode pretrain --use-clip --epoch 200 --wd 0.0 --num-transf 24 --base-dim 1152 --num-head 16 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0"
+#Big PET  Total params: 463.68M
 
-# #Big PET Total params: 178.11M
-# cmd="omnilearned train  -o ./ --save-tag pretrain --dataset pretrain --use-pid --use-add --num-classes 201 --batch 64 --iterations 1000 --mode pretrain --use-clip --epoch 200 --wd 0.0 --num-transf 12 --base-dim 768 --num-head 12 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0"
+# cmd="omnilearned train  -o /pscratch/sd/v/vmikuni/PET/checkpoints/ --save-tag pretrain_l --dataset pretrain --use-pid --use-add --use-clip --num-classes 201 --batch 8 --iterations 1000 --mode pretrain --epoch 1000 --wd 0.01 --num-transf 28 --base-dim 1152 --num-head 16 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0"
+
+#Medium PET Total params: 62.61M
+
+#cmd="omnilearned train  -o /pscratch/sd/v/vmikuni/PET/checkpoints/ --save-tag pretrain_m --dataset pretrain --use-pid --use-add --use-clip --num-classes 201 --batch 16  --iterations 1000 --mode pretrain  --epoch 250 --wd 0.01 --num-transf 12 --base-dim 512 --num-head 8 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0 --wandb"
+
+#cmd="omnilearned train  -o /pscratch/sd/v/vmikuni/PET/checkpoints/ --save-tag top_m --dataset top   --epoch 15 --fine-tune --pretrain-tag pretrain_m --lr 5e-6 --lr-factor 0.1 --base-dim 512 --num-transf 12  --attn-drop 0.1 --mlp-drop 0.1 --wd 0.3"
+
+#Small PET Total params: 0.9M
+
+#cmd="omnilearned train  -o ./ --save-tag pretrain_s --dataset pretrain --use-pid --use-add --use-clip --num-classes 201 --batch 64 --iterations 1000 --mode pretrain  --epoch 1000 --wd 0.1 --num-transf 6 --base-dim 96 --num-head 8 --num-workers 32 --feature-drop 0.1 --wandb --resuming"
+#cmd="omnilearned train  -o ./ --save-tag pretrain_s_gen --dataset pretrain --use-pid --use-add --num-classes 201 --batch 64 --iterations 1000 --mode pretrain  --epoch 1000 --wd 0.1 --num-transf 6 --base-dim 96 --num-head 8 --num-workers 32 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0 --wandb --resuming"
+#cmd="omnilearned train  -o ./ --save-tag pretrain_s_class --dataset pretrain --use-pid --use-add --num-classes 201 --batch 128 --iterations 1000 --mode classifier  --epoch 500 --wd 0.01 --num-transf 6 --base-dim 64 --num-head 8 --num-workers 32 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0 --wandb"
+
+#cmd="omnilearned train  -o ./ --save-tag top_s --dataset top   --epoch 15 --fine-tune --pretrain-tag pretrain_s --lr 5e-4 --lr-factor 1.0 --base-dim 96  --attn-drop 0.1 --mlp-drop 0.1 --wd 0.3"
+#cmd="omnilearned train  -o ./ --save-tag top_s_gen --dataset top   --epoch 15 --fine-tune --pretrain-tag pretrain_s_gen --lr 5e-4 --lr-factor 1.0 --base-dim 96  --attn-drop 0.1 --mlp-drop 0.1  --wd 0.3"
+#cmd="omnilearned train  -o ./ --save-tag top_s --dataset top   --epoch 15  --lr 5e-4 --base-dim 64"
 
 
-#Medium PET Total params: 58.42M  
-cmd="omnilearned train  -o ./ --save-tag pretrain_medium --dataset pretrain --use-pid --use-add --use-clip --num-classes 201 --batch 32 --lr 3e-5 --iterations 1000 --mode pretrain  --epoch 200 --wd 0.1 --num-transf 12 --base-dim 512 --num-head 8 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0 --resuming"
-
-#cmd="omnilearned train  -o ./ --save-tag jetclass_medium --dataset jetclass --use-pid --use-add --num-classes 10 --batch 32 --lr 3e-4 --iterations 1000 --mode classifier  --epoch 200 --wd 0.1 --num-transf 12 --base-dim 512 --num-head 8 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0"
-
-
-#cmd="omnilearned train  -o ./ --save-tag top_medium --dataset top  --num-classes 2 --batch 64 --lr 3e-5  --mode classifier  --epoch 15 --wd 0.3 --num-transf 12 --base-dim 512 --num-head 8  --attn-drop 0.1 --mlp-drop 0.1 --fine-tune --pretrain-tag pretrain_medium"
-
-
-# #Small PET Total params: 0.9M
-# cmd="omnilearned train  -o ./ --save-tag pretrain --dataset pretrain --use-pid --use-add --num-classes 201 --batch 64 --iterations 1000 --mode pretrain --use-clip --epoch 200 --wd 0.0 --num-transf 6 --base-dim 64 --num-head 8 --feature-drop 0.1 --attn-drop 0.0 --mlp-drop 0.0"
-
-
-#TOP
-#cmd="omnilearned train  -o ./ --save-tag top --dataset top  --num-classes 2 --epoch 15 --wd 0.3 --num-transf 24 --base-dim 64 --num-head 8 --feature-drop 0.0 --attn-drop 0.1 --mlp-drop 0.1"
+#cmd="omnilearned train  -o ./ --save-tag top_s --dataset top   --epoch 15 --lr 5e-5  --base-dim 64 --num-transf 6  --attn-drop 0.1 --mlp-drop 0.1 --wd 0.3"
+#cmd="omnilearned train  -o ./ --save-tag top_s --dataset top   --epoch 15 --lr 5e-5  --base-dim 1152 --num-transf 28  --attn-drop 0.1 --mlp-drop 0.1 --wd 0.3 --num-head 16 --batch 32"
 
 set -x
 srun -l -u \
